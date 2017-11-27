0a9a2dd4c748d7d396cabbb6ec27b7922b013a28
Võ Lê Thanh Phú <volethanhphu174@gmail.com>
2017-11-27 11:30:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -38,7 +38,10 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array
 }
 
-function 
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+}
+
 
 
 
